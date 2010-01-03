14a532916b66df63cb70bf180072be76e21f7252
gfx <g.psy.va@gmail.com>
2010-01-03 15:35:58 +0900

    First version

--- lib/Task/BeLike/GFUJI.pm
+++ lib/Task/BeLike/GFUJI.pm
@@ -2,31 +2,4 @@ package Task::BeLike::GFUJI;
 use strict;
 use warnings;
 our $VERSION = '0.01';
-
 1;
-__END__
-
-=head1 NAME
-
-Task::BeLike::GFUJI -
-
-=head1 SYNOPSIS
-
-  use Task::BeLike::GFUJI;
-
-=head1 DESCRIPTION
-
-Task::BeLike::GFUJI is
-
-=head1 AUTHOR
-
-Goro Fuji E<lt>gfuji@cpan.orgE<gt>
-
-=head1 SEE ALSO
-
-=head1 LICENSE
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
